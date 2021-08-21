e75a9c8a582a801788e137f7e89e3191e2452c1e
appcodeth <appcodetraining@gmail.com>
2021-08-21 19:08:22 +0700

    add wsgi connector

--- mems/wsgi.py
+++ mems/wsgi.py
@@ -1,7 +1,8 @@
 import os
+import sys
+
+sys.path.append("/var/www/website/envs/mems")
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mems.settings')
-
 application = get_wsgi_application()
