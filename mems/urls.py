9f21f42df0a110051d71147362a2940acd9046f9
appcodeth <appcodetraining@gmail.com>
2021-08-21 18:54:07 +0700

    revise for django version 1

--- mems/urls.py
+++ mems/urls.py
@@ -1,19 +1,17 @@
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from django.urls import path, re_path, include
-from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url, include
+from django.http import HttpResponse
 
 SITEMAPS = {}
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('', include(('app.urls', 'app'), namespace='app')),
-    re_path(r'^robots.txt$', lambda r: HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')),
-    re_path(r'^sitemap.xml$', sitemap, {'sitemaps': SITEMAPS}, name='sitemap'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'^', include('app.urls', namespace='app')),
+    url(r'^robots.txt$', lambda r: HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_title = 'MEMs'
